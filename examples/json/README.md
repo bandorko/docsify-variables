e96a004d2794c66611e704be36008720e2c9f60a
Balazs Andorko <balazs.andorko@gmail.com>
2019-06-29 01:43:20 +0200

    json support

---
+++ examples/json/README.md
@@ -0,0 +1,10 @@
+# Example
+## content of the variables.json file
+[filename](variables.json ':include :type=code xml')
+
+## you can use fully specified path to the variable
+This will be changed to the value of the **&lt;foo&gt;** tag in the **&lt;var&gt;** tag : ${var.foo}
+## you can use JSONPath
+This will be changed to the value of the **&lt;foo&gt;** tag in the **&lt;var&gt;** tag : ${$.var.foo}
+
+This will be the value of the second **&lt;item&gt;** tag of the list: ${$..item[1]}
--- src/index.js
+++ src/index.js
@@ -1,46 +1,83 @@
-
-var xmlDoc = null;
+var variablesObj = null;
 var variablesFile;
 var nsResolver = null;
+var variablesFileType;
+
 
 function resolveVar(v){
-    if (variablesFile && xmlDoc == null){
-        xhttp = new XMLHttpRequest();
-        xhttp.open("GET", variablesFile, false);
-        xhttp.send(null);
-        str = xhttp.response.replace(/(<[\s\S]*)xmlns=\"[^"]*\"([^>]*>)/g, "$1$2") //remove default namespace
-        xmlDoc = stringToXML(str);
+    if (variablesFile && variablesObj == null){
+        initVariablesObj();
     }
-    variable = v
+    if (variablesFileType == "xml"){
+        return resolveXMLVar(v);
+    }else if (variablesFileType == "json"){
+        return resolveJSONVar(v);
+    }
+    return "${"+v+"}"
+}
+
+function resolveJSONVar(v){
+    variable = v;
+    if (!variable.startsWith("$")){
+        variable = "$."+variable;
+    }
+    try{
+        var jp = require('jsonpath-plus').JSONPath;
+        ret = jp({path: variable, json: variablesObj});
+    }catch(e){
+        ret = "${"+v+"}"
+    }
+    return ret;
+}
+
+function resolveXMLVar(v){
+    variable = v;
     if (!variable.startsWith("/")){
         variable = "/"+variable.replace(/(\.)/g,"/");
     }
     try{
-      ret = xmlDoc.evaluate(variable, xmlDoc, null, XPathResult.ANY_TYPE,null).iterateNext().childNodes[0].nodeValue;
+      ret = variablesObj.evaluate(variable, variablesObj, null, XPathResult.ANY_TYPE,null).iterateNext().childNodes[0].nodeValue;
     }catch(e){
       ret = "${"+v+"}"
     }
     return ret
 }
 
-function stringToXML(string){
-    var xmlDoc=null;
-    if (window.DOMParser)
-    {
-        parser=new DOMParser();
-        xmlDoc=parser.parseFromString(string,"text/xml");
-    }
-    else // Internet Explorer
-    {
-        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-        xmlDoc.async="false";
-        xmlDoc.loadXML(string);
-    }
-    return xmlDoc;
+function initVariablesObj(){
+    xhttp = new XMLHttpRequest();
+    xhttp.open("GET", variablesFile, false);
+    xhttp.send(null);
+    str = xhttp.response;
+    variablesObj = stringToObj(str,variablesFileType);
+}
+
+function stringToObj(str, type){
+    var variablesObj=null;
+    if (type == "xml"){
+        str = str.replace(/(<[\s\S]*)xmlns=\"[^"]*\"([^>]*>)/g, "$1$2") //remove default namespace
+        if (window.DOMParser)
+        {
+            parser=new DOMParser();
+            variablesObj=parser.parseFromString(str,"text/xml");
+        }
+        else // Internet Explorer
+        {
+            variablesObj=new ActiveXObject("Microsoft.XMLDOM");
+            variablesObj.async="false";
+            variablesObj.loadXML(str);
+        }
+    }else if (type == "json"){
+        variablesObj = JSON.parse(str);
+    }   
+    return variablesObj;
 }
 
 function install(hook,vm){
     variablesFile = vm.config.variablesFile;
+    variablesFileType = vm.config.variablesFileType;
+    if (!variablesFileType) variablesFileType=variablesFile.split('.').pop();
+    if (!variablesFileType) variablesFileType = "xml";
+    variablesFileType = variablesFileType.toLowerCase();
 
     hook.afterEach(function(html, next) {
         next(html.replace(/\${([^\}]*)}/g, function(a, b){return resolveVar(b)}))
